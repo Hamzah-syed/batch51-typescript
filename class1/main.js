4c9f8769056175b6b79e0cf87515f585d324f635
hamzah-syed <hamzah.syed17@gmail.com>
2023-11-19 10:45:05 +0000

    https://github.com/Hamzah-syed/batch51-typescript.git

---
+++ class1/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let x = 24;
+let h = "hamzah";
+let a = 51;
+let b = "ammaa";
+// console.log("hi my name is "+h+",my age is"+x)
+console.log(a + b);
---
+++ class2/main.js
@@ -0,0 +1,59 @@
+"use strict";
+// let powder = "Lal mirch powder";
+// powder = "Haldi";
+// console.log("Hello");
+// console.log(powder);
+// let fullName:string = "Hamzah";
+// fullName = "okasha"
+// fullName = "ali"
+// fullName = "hamzah"
+// console.log(fullName);
+// let fullName = "Hamzah Syed";
+// let age = 22;
+// let bulb = true;
+// let bulb = false;
+// let fullName;
+// console.log(fullName);
+// fullName = "Hamzah" 
+// let firstName = "Hamzah";
+// let lastName = "Syed";
+// // console.log(firstName + " " + lastName);
+// console.log(`my name is ${firstName} ${lastName}`);
+// let qty = 8;
+// console.log(qty + 10);
+// console.log(qty - 1);
+// console.log(qty * 12000);
+// console.log(qty * 12000);
+// console.log(qty / 3)
+// let str1 = "1";
+// let str2 = "2";
+// console.log(str1 + str2); 
+// // let n1 = 2;
+// // let n2 = 3;
+// // console.log(n1 ** n2)
+// console.log(qty % 3)
+// let age = 22;
+// // age += 10;
+// age -= 10;
+// console.log(age);
+// let n = 5;
+// console.log(n); // 5
+// n += 5;
+// console.log(n); // 10
+// n -= 5;
+// console.log(n); // 5
+// let age = 22;
+// console.log( age == 22 );
+// console.log( age === '22' );
+// console.log(age != 22);
+// console.log(age > 10);
+// console.log(age >= 22);
+// console.log(age <= 22);
+// let n = 5;
+// console.log(n == 5); // True
+// console.log(n === 5); // True
+// // console.log(n != 5); // False
+// // console.log(n > 8); // False
+// console.log(n < 5); // False
+// // console.log(n >= 8); // Flase
+// console.log(n <= 8); // True
