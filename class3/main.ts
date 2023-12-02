1999948f650e9226b86ce2d960d2be7b7dd32ff6
hamzah-syed <hamzah.syed17@gmail.com>
2023-12-02 07:02:00 +0000

    added class 4

---
+++ class3/main.ts
@@ -0,0 +1,220 @@
+// let username:any
+// username="Sir Mustafa"
+// username=456
+// console.log(username)
+
+
+
+
+// let username:string="Sir Mustafa"
+// username=456
+// console.log(username)
+
+
+
+
+
+// let username:string="Sir Mustafa"
+// username="Sir Gulam Mustafa"
+// console.log("Sir Mustafa")
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Taking Decision");
+// let budget = 40000
+// let cycle = 45000
+// if (cycle < budget) {
+//         console.log("Buy Cycle")
+// }
+// else {
+//     console.log("I need to build my skills to earn more")
+// }
+
+
+
+
+
+
+
+
+// console.log("Taking Decision");
+// let budget = 40000
+// let cycle = 45000
+// if(budget<cycle){
+//     console.log("I do not afford to purchase cycle")
+// }else {
+//     console.log("i can  afford to purchase cycle")
+// }
+
+
+
+
+
+
+
+
+
+
+// // single = assign
+// let num:any = "3"
+// let username = "Ameen"
+// console.log(num) // 3
+// // double == match
+// console.log(num == 2 || username) // false
+// // triple === match - data type
+// console.log(num === 2) // false
+
+
+
+
+
+
+
+
+// let num:any = 2
+// let username = "ameen"
+// console.log(num) // 3
+// console.log(num == 2 && username=="Ameen") // false
+
+
+
+
+
+
+
+
+
+// // single = assign
+// let car:string="BMW"
+// let CarColor:string="black"
+
+// if(CarColor != "red"){
+//     console.log("Color is not red")
+// }
+
+
+// // single =
+// // double ==
+// // triple === 
+
+
+
+
+
+
+// // single = assign
+// let car:string="XYZ"
+// let CarColor:string="black"
+
+// console.log(car)
+
+// // single =
+// // double ==    !=
+// // triple ===   !==
+
+
+
+
+// console.log("Taking Decision");
+// let budget = 40000
+// let cycle = 40000
+// let color = "red"
+// if (((color=="blue" || color =="black")  && cycle <= budget) ||  color =="red") {
+//         console.log("Buy Cycle")
+// }
+// else {
+//     console.log("I need to build my skills to earn more")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Taking Decision");
+// let budget = 40000
+// let cycle = 40000
+// let color = "blue"
+// if (cycle <= budget) {
+//     if (color == "black") {
+//         console.log("Buy Cycle")
+//     } else {
+//         console.log("Color is not matched")
+//     }
+// }
+// else {
+//     console.log("I need to build my skills to earn more")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Taking Decision");
+// let budget=40000
+// let cycle = 40000
+// let color="black"
+// if(cycle <= budget){
+//     if(color=="black"){
+//         console.log("Buy Cycle")
+//     }
+// }
+// else{
+//     console.log("I need to build my skills to earn more")
+// }
+
+
+
+
+
+
+
+
+// console.log("Taking Decision");
+// let raita=true
+// let biscuit=true
+// if(raita){
+//     console.log("Buy Samosa")   
+// }
+// else if(biscuit){
+//     console.log("Buy Biscuit")   
+// }
+// else{
+//     console.log("Don't buy")
+// } 
+
+
+
+
+
+// let salary=40000
+// let monthlyexpense = 80000
+// if (salary===40000){
+//     console.log("you have not enough money")
+// } else {
+//     console.log("you have enough money ")
+// }
+
---
+++ class4/main.js
@@ -0,0 +1,70 @@
+"use strict";
+// let firstName = "Hamzah"
+// let lastName = "Syed";
+//               // True                  // False 
+// if (firstName === "Hamzah" || firstName === "Okasha" ) {
+//     console.log("You're not allowed")
+// }
+// else {
+//     console.log("You're allowed")
+// }
+// function greet() {
+//     console.log("Hello Hamzah")
+// }
+// function sum() {
+//     console.log(2 + 2)
+// }
+// greet();
+// sum();
+// greet();
+// function greet(name: string) {
+//     console.log("Hello", name);
+// }
+// greet("Hamzah");
+// greet("Ali");
+//             5            10
+// function sum(num1:number, num2:number){
+//     console.log(num1 + num2)
+// }
+// sum(5, 10);
+// function sum(num1: number, num2: number) {
+//     // console.log(num1 + num2);
+//     return num1 + num2;
+// }
+// //   15
+// let ans = sum(5, 10); // 15
+// function multiply(num1: number) {
+//     console.log(num1 * 2)
+// }
+//         //15
+// multiply(ans)
+// multiply()
+// let fruit1 = "Apple";
+// let fruit2 = "Orange";
+// let fruit3 = "Banana";
+// let fruit4 = "Pineapple";
+// let subjects:string[] = [ "Math", "Science", "English", "History"];
+// let ages:number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let a = [true, false, true];
+//              0         1         2          3  // Index number
+// let fruits = ["Apple", "Orange", "Banana", "Pineapple"];
+// console.log(fruits[1]);
+// console.log(fruits[2], fruits[3] );
+// let fruits = ["Apple", "Orange", "Banana", 4];
+// let menu = ["Kofta", "Kheer", "Biryani", "Gajar ka Halwa"];
+// let myFirstPopped = menu.pop();
+// console.log(myFirstPopped);
+// let pushedReturn = menu.push("Chicken Tikka", "Chicken Karahi");
+// console.log(pushedReturn);
+// menu.pop();
+// console.log(menu);
+// let menu = ["Kofta", "Kheer", "Biryani", "Gajar ka Halwa"];
+// let shiftedElement = menu.shift();
+// let len  = menu.unshift("Chicken Tikka");
+// console.log(len);
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let a = fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(a);
