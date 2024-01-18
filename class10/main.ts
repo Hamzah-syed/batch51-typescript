f40cb82b0167dd3c2668ec42fd9688e142a2c05e
hamzah-syed <hamzah.syed17@gmail.com>
2024-01-18 21:35:48 +0500

    added class 11

---
+++ class10/main.ts
@@ -0,0 +1,156 @@
+// function sum(num1: number, num2: number): number {
+//     return num1 + num2;
+// }
+
+// let result = sum(3, 6);
+
+
+// type Sum = (num1: number, num2: number) => number;
+
+// let add: Sum = function (num1, num2) {
+//     return num1 + num2;
+// }
+
+// let result = add('3', 6);
+// console.log(result);
+
+// Arrow Function
+// let sum = (num1: number, num2: number): number => num1 + num2;
+
+
+// let result = sum(3, 6); // 9
+// console.log(result);
+
+
+
+// let fullName = (firstName: string, lastName?: string) => {
+
+//     if (lastName === undefined) {
+//         return firstName
+//     }
+//     else {
+//         return firstName + ' ' + lastName;
+//     }
+
+// };
+
+
+// let fullN = fullName('Ali', 'khan'); 
+
+// console.log(fullN);
+
+// let fullName = (firstName: string, lastName = "Khan") => {
+//     // console.log(firstName, lastName);
+
+//     return firstName + ' ' + lastName;
+// };
+
+
+// let fullN = fullName('Hamzah'); // undefined    
+
+// console.log(fullN);
+
+// Rest Operator
+// console.log("hello world")
+
+// let makeMessage = (name: string, ...message: string[]) => {
+
+//     console.log(message.join(" ")); 
+
+// }
+
+// makeMessage("Ali", "Hello", "World", "How", "are", "you")
+
+
+
+
+// function add(arg1: number, arg2: number): number;//option 2
+// function add(arg1: boolean, arg2: boolean): boolean;//option 3
+//this is not part of the overload list, 
+//so it has only three overloads
+
+
+
+
+
+// function add(arg1: number, arg2: string): string; //option 1
+// function add(arg1: string, arg2: number): string; //option 2
+// function add(arg1: string, arg2: string): string; //option 2
+
+// function add(arg1: any, arg2: any): any {
+//     return arg1 + arg2;
+// }
+
+// add("hamzah", "syed");
+// add(2, "Hamzah")
+// add(2, 3)
+// add("hamzah", 5)
+
+// function add(arg1: string, arg2: string): string;//option 1
+// function add(arg1: number, arg2: number): number;//option 2
+// function add(arg1: boolean, arg2: boolean): boolean;//option 3
+// //this is not part of the overload list, 
+// //so it has only three overloads
+// function add(arg1: any, arg2: any): any {
+//     return arg1 + arg2;
+// }
+
+// let result = add(4, 7);
+// console.log(result);
+
+
+
+// let fruits: [string, string, number] = ["Orange", "Mango", 2];
+
+// fruits.push("Apple");
+
+
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+    {
+        type:"number",
+        name:"userID",
+        message:"please enter your userID"
+    },
+    {
+        type:"number",
+        name:"userPin",
+        message:"please enter your userPin"
+    },{
+        type:"list",
+        name:"AccountType",
+        choices:["Current Account","Saving Account"],
+        message:"please select your account type"
+    },{
+        type:"list",
+        name:"transactionType",
+        choices:["fastCash","withdraw"],
+        message:"please select your transaction type"
+    },{
+        type:"list",
+        name:"amount",
+        choices:[500,1000,5000,10000],
+        message:"please select your amount",
+        when(answers){
+            return answers.transactionType === "fastCash"
+        }
+    },{
+        type:"number",
+        name:"amount",
+        message:"please enter your amount",
+        when(answers){
+            return answers.transactionType === "withdraw"
+        }
+    },
+])
+
+if(answers.userID && answers.userPin){
+    const balance = Math.floor(Math.random()*1000000)
+    const enteredamount = answers.amount
+    if(enteredamount <= balance){
+       const remaining = balance - enteredamount
+       console.log( `${enteredamount} withdraw sucessful.. and your remaining balance is ${remaining}`)
+    }
+
+}
---
+++ class11/main.js
@@ -0,0 +1,79 @@
+// class House {
+//     readonly numberOfRooms: number;
+//     readonly numberOfBathrooms: number;
+//     constructor(nOfRooms: number, nOfBathrooms: number) {
+//         // this // this = {}
+//         this.numberOfBathrooms = nOfBathrooms
+//         this.numberOfRooms = nOfRooms
+//     }
+//     doorBell() {
+//       console.log('bell pressed'); 
+//     }
+// }
+// // {numberOfBathrooms:4,numberOfRooms:2}
+// let house1 = new House(4, 2);
+// let house2 = new House(3, 1);
+// house1.doorBell()
+// house1.numberOfBathrooms = 10
+// console.log(house1);
+// console.log(house2);
+// console.log(house1.numberOfRooms)
+// let house2 = {
+//     numberOfRooms: 4
+// }
+// let house3 = {
+//     numberOfRooms: 4
+// }
+// let house4 = {
+//     numberOfRooms: 4
+// }
+// Inheritance
+// class Person {
+//     name: string = 'okasha'
+//     constructor(name: string) {
+//         // this.name = name
+//     }
+// }
+// class Person1 extends Person {
+//     salary: number = 20000
+//     // constructor(name: string, salary: number) {
+//     //     // this.name = name
+//     //     this.salary = salary
+//     // }
+// }
+// class Person2 {
+//     hobby: string
+//     constructor(name: string, hobby: string) {
+//         // this.name = name
+//         this.hobby = hobby
+//     }
+// }
+// let person1 = new Person1('bilal')
+// console.log(person1);
+// let person2 = new Person2('okasha', 'cricket')
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+class Teacher extends Person {
+    constructor(name, age, salary) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+// class Student extends Person {
+//     rollNo: string
+//     constructor(name: string, age: number, rollNo: string) {
+//         this.name = name
+//         this.age = age
+//         this.rollNo = rollNo
+//     }
+// }
+let teacher = new Teacher('hamzah', 20, 20000);
+console.log(teacher);
+export {};
+// let student = new Student("ali", 23, "123445")
